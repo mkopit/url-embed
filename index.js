f8c179321e3cb2f1f189f3c9742b007d96d459ab
Matt Kopit <matt@kopit.com>
2016-03-01 01:58:37 -0500

    Moved directory class loading functions to utils.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/** @ignore */
+let utils = require('./lib/utils');
+
 module.exports.EmbedEngine = require('./lib/classes/EmbedEngine');
 module.exports.URLEmbedProvider = require('./lib/classes/URLEmbedProvider');
 module.exports.OEmbedProvider = require('./lib/classes/OEmbedProvider');
+module.exports.defaultProviderClasses = {};
+
+let providerList = utils.loadClassesFromDirectory(__dirname + '/lib/classes/default_providers');
+
+for (let i = 0; i < providerList.length; i++) {
+  let provider = providerList[i];
+  module.exports.defaultProviderClasses[provider.name] = provider;
+}
