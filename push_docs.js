e0a3bd368699d4da0b85f49a186bff97a479c95f
Matt Kopit <matt@kopit.com>
2016-03-06 04:00:00 -0500

    Making linter happy

--- push_docs.js
+++ push_docs.js
@@ -1,11 +1,11 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'docs/esdoc'), function(err) {
+ghpages.publish(path.join(__dirname, 'docs/esdoc'), function (err) {
   if (err) {
     console.error('An error occurred while pushing docs: ');
     console.error(err.stack);
   } else {
-    console.log('gh-pages published.\n')
+    console.log('gh-pages published.\n');
   }
 });
